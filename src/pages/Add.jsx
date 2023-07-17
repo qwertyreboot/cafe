03186c70af2866e03165a98951433ea5a6ea0d5f
khavinshankar <khavinshankar@gmail.com>
2023-07-17 19:34:07 +0530

    added filters

--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -1,34 +1,109 @@
 import Input from "../components/Input";
 import Navbar from "../components/Navbar";
+import { useNavigate } from "react-router-dom";
+import { useReducer } from "react";
+
+const initialState = {
+  name: "",
+  image: "",
+  price: 0,
+  rating: 0,
+  time: 0,
+};
+
+function reducer(state, { action, data }) {
+  switch (action) {
+    case "set_name": {
+      return { ...state, name: data };
+    }
+    case "set_price": {
+      return { ...state, price: Number(data) };
+    }
+    case "set_image": {
+      return { ...state, image: data };
+    }
+    case "set_rating": {
+      return { ...state, rating: Number(data) };
+    }
+    case "set_time": {
+      return { ...state, time: Number(data) };
+    }
+    case "reset": {
+      return { ...initialState };
+    }
+    default:
+      return state;
+  }
+}
 
 export default function Add() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const navigate = useNavigate();
+
+  const addDish = (dish) => {
+    const localDishes = JSON.parse(localStorage.getItem("dishes") ?? "[]");
+    localStorage.setItem("dishes", JSON.stringify([dish, ...localDishes]));
+  };
+
   return (
     <>
       <Navbar />
       <h1 className="text-3xl font-bold mt-16 text-center text-gray-600">
         Add Dish
       </h1>
-      <form className="w-96 m-auto mt-10">
-        <Input label="Name" name="name" placeholder="Enter dish name" />
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          addDish({ id: new Date(), ...state });
+          dispatch({ action: "reset" });
+          navigate("/");
+        }}
+        className="w-96 m-auto mt-10"
+      >
         <Input
+          value={state.name}
+          onChange={(e) =>
+            dispatch({ action: "set_name", data: e.target.value })
+          }
+          label="Name"
+          name="name"
+          placeholder="Enter dish name"
+        />
+        <Input
+          value={state.image}
+          onChange={(e) =>
+            dispatch({ action: "set_image", data: e.target.value })
+          }
           label="Image"
           name="image"
           type="url"
           placeholder="Enter image url"
         />
         <Input
+          value={state.price}
+          onChange={(e) =>
+            dispatch({ action: "set_price", data: e.target.value })
+          }
           label="Price"
           name="price"
           type="number"
           placeholder="Enter delevery time in ruppes"
         />
         <Input
+          value={state.time}
+          onChange={(e) =>
+            dispatch({ action: "set_time", data: e.target.value })
+          }
           label="Time"
           name="time"
           type="number"
           placeholder="Enter delevery time in minutes"
         />
         <Input
+          value={state.rating}
+          onChange={(e) =>
+            dispatch({ action: "set_rating", data: e.target.value })
+          }
           label="Rating"
           name="rating"
           type="number"
@@ -36,6 +111,12 @@ export default function Add() {
           max={5}
           placeholder="Enter a rating between 0 to 5"
         />
+
+        <div className="mt-10 flex justify-end">
+          <button className="px-4 py-2 rounded bg-green-500 text-white hover:bg-green-700 shadow">
+            Submit
+          </button>
+        </div>
       </form>
     </>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,49 +1,89 @@
+import { useEffect, useState } from "react";
+
 import DishCard from "../components/DishCard";
+import Input from "../components/Input";
+import Modal from "../components/Modal";
+import MultiRangeSlider from "multi-range-slider-react";
 import Navbar from "../components/Navbar";
 
 export default function Home() {
-  const dishes = [
-    {
-      name: "Pizza",
-      image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQocrHavgFrfLPtHnenHhX5rhZHL4wOLcNA5MeflQRn9wQx2Y8nlptO9Gs6ss8J9oa5yPA&usqp=CAU",
-      price: 150,
-      time: 30,
-      rating: 4.2,
-    },
-    {
-      name: "Burger",
-      image:
-        "https://cdn.pixabay.com/photo/2016/03/14/10/32/food-in-trains-1255283_1280.jpg",
-      price: 150,
-      time: 30,
-      rating: 4.2,
-    },
-    {
-      name: "Burger2",
-      image:
-        "https://d1rgpf387mknul.cloudfront.net/products/PLP/web/2x_web_20230313180446431450_482x264jpg",
-      price: 150,
-      time: 30,
-      rating: 4.2,
-    },
-    {
-      name: "Briyani",
-      image:
-        "https://media.istockphoto.com/id/1492813806/photo/briyani-rice.jpg?b=1&s=170667a&w=0&k=20&c=GGf-z4S_ZxFcv21Ddh9aLK7J6HHEtxouN9OfKcG04h4=",
-      price: 90,
-      time: 10,
-      rating: 4.5,
-    },
-  ];
+  const [dishes, setDishes] = useState([]);
+  const [showFilters, setShowFilters] = useState(false);
+
+  const [filters, setFilters] = useState({
+    rating_min: 0,
+    rating_max: 5,
+  });
+
+  useEffect(() => {
+    const localDishes = JSON.parse(localStorage.getItem("dishes") ?? "[]");
+    setDishes(localDishes);
+  }, []);
+
+  useEffect(() => {
+    const localDishes = JSON.parse(localStorage.getItem("dishes") ?? "[]");
+    setDishes(
+      localDishes.filter(
+        (dish) =>
+          dish.rating >= filters.rating_min && dish.rating <= filters.rating_max
+      )
+    );
+  }, [filters]);
 
   return (
     <>
       <Navbar />
-      <div className="flex flex-wrap w-[70%] m-auto mt-16 gap-4 items-center justify-center">
-        {dishes.map((dish) => (
-          <DishCard key={dish.name} dish={dish} />
-        ))}
+      <div className="w-[70%] m-auto mt-16">
+        <div className="flex items-center justify-evenly gap-4">
+          <Input
+            onChange={(e) => {
+              const searchTerm = e.target.value.trim().toLowerCase();
+              const localDishes = JSON.parse(
+                localStorage.getItem("dishes") ?? "[]"
+              );
+              setDishes(
+                localDishes.filter((dish) =>
+                  dish.name.trim().toLowerCase().includes(searchTerm)
+                )
+              );
+            }}
+            className="flex-1 w-full"
+            placeholder="Start typing to search by name"
+          />
+
+          <button
+            type="button"
+            onClick={() => setShowFilters(true)}
+            className="flex-initial w-48 rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+          >
+            Open Filters
+          </button>
+        </div>
+        <Modal
+          show={showFilters}
+          onClose={() => setShowFilters(false)}
+          title="Filters"
+        >
+          <MultiRangeSlider
+            min={1}
+            max={5}
+            step={0.5}
+            minValue={filters.rating_min}
+            maxValue={filters.rating_max}
+            onInput={(e) => {
+              setFilters({
+                ...filters,
+                rating_max: e.maxValue,
+                rating_min: e.minValue,
+              });
+            }}
+          />
+        </Modal>
+        <div className="flex flex-wrap gap-4 mt-8 items-center justify-center">
+          {dishes.map((dish) => (
+            <DishCard key={dish.name} dish={dish} />
+          ))}
+        </div>
       </div>
     </>
   );
